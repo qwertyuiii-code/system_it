b502905f06befdf74309c0e30937b34027b45ec9
Alexey Shelby <shelbyhell@proton.me>
2025-03-05 17:59:10 +0400

    php: db: update password.
        [1] - you fuckin retard.
    
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'it_management';
 $username = 'root';
-$password = '1';
+$password = 'newpassword';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
