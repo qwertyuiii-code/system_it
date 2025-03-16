824a3767fb873430377e6d123d2f2e49bdbf1414
Alexey Shelby <shelbyhell@proton.me>
2025-03-16 10:06:35 +0400

    sql: Import `user` column.
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- it_management.sql
+++ it_management.sql
@@ -3,6 +3,7 @@ USE it_management;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user') DEFAULT 'user'
